be92205edc8c70b78f0ac432369d9a6b23a01ed2
Marco Prontera <prontera.marco.pm@gmail.com>
2021-06-17 14:46:04 +0200

    Add code examples

---
+++ test/Example/example.test.ts
@@ -0,0 +1,12 @@
+import {expect} from 'chai';
+
+describe("Example suit test", () => {
+
+    it("Example test", (done: () => void): void => {
+
+        expect(true).to.be.true;
+        done();
+
+    });
+
+});
