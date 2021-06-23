3a73cecdc974a15c097f791ad298e32a89dc0fd2
Marco Prontera <prontera.marco.pm@gmail.com>
2021-06-23 12:25:19 +0200

    Add TCModelService

---
+++ src/Service/TCModelService.ts
@@ -0,0 +1,90 @@
+import {GVL, TCModel, TCString} from '@iabtcf/core';
+import TCStringService from './TCStringService';
+import CmpSupportedLanguageProvider from './CmpSupportedLanguageProvider';
+
+/**
+ * TCModelService.
+ */
+class TCModelService {
+
+    private readonly cmpId: number;
+    private readonly cmpVersion: number;
+    private readonly isServiceSpecific: boolean;
+    private globalVendorList: GVL;
+    private tcStringService: TCStringService;
+    private cmpSupportedLanguageProvider: CmpSupportedLanguageProvider;
+
+    /**
+     * Constructor.
+     *
+     * @param {TCStringService} tcStringService
+     * @param {CmpSupportedLanguageProvider} cmpSupportedLanguageProvider
+     * @param {number} cmpId
+     * @param {number} cmpVersion
+     * @param {boolean} isServiceSpecific
+     * @param {GVL} globalVendorList
+     */
+    constructor(
+        tcStringService: TCStringService,
+        cmpSupportedLanguageProvider: CmpSupportedLanguageProvider,
+        cmpId: number,
+        cmpVersion: number,
+        isServiceSpecific: boolean,
+        globalVendorList: GVL,
+    ) {
+
+        this.tcStringService = tcStringService;
+        this.cmpSupportedLanguageProvider = cmpSupportedLanguageProvider;
+        this.cmpId = cmpId;
+        this.cmpVersion = cmpVersion;
+        this.isServiceSpecific = isServiceSpecific;
+        this.globalVendorList = globalVendorList;
+
+    }
+
+    /**
+     * Build the TCModel from a TCString if is persisted
+     * with the GVL used to generate it, otherwise build
+     * a new TCModel with new GVL.
+     *
+     * @param {string} tcString
+     * @return {Promise<TCModel>}
+     */
+    public async fetchAndBuildTCModel(tcString: string): Promise<TCModel> {
+
+        const encodedString = tcString;
+
+        let tcModel = new TCModel(this.globalVendorList);
+
+        // Some fields will not be populated until a GVL is loaded
+        const promise = tcModel.gvl.readyPromise
+            .then(() => {
+
+                if (this.tcStringService.isValidTCString(encodedString)) {
+
+                    const builtTCModel = TCString.decode(encodedString);
+                    builtTCModel.gvl = tcModel.gvl;
+                    tcModel = builtTCModel;
+
+                }
+
+                tcModel.cmpId = this.cmpId;
+                tcModel.cmpVersion = this.cmpVersion;
+                tcModel.isServiceSpecific = this.isServiceSpecific;
+
+                tcModel.gvl.changeLanguage(this.cmpSupportedLanguageProvider.getCurrentLanguageForCmp());
+
+            })
+            .then(() => {
+
+                return tcModel;
+
+            });
+
+        return await promise;
+
+    }
+
+}
+
+export default TCModelService;
