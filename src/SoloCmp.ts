555bfdaf82fde113eaad6918f533520a69ac0fa8
Marco Prontera <prontera.marco.pm@gmail.com>
2021-06-23 12:25:51 +0200

    Inject new service TCModelService

--- src/SoloCmp.ts
+++ src/SoloCmp.ts
@@ -5,6 +5,8 @@ import Cookie from './Service/Cookie';
 import CmpConfigurationProvider from './Service/CmpConfigurationProvider';
 import CmpSupportedLanguageProvider from './Service/CmpSupportedLanguageProvider';
 import TCStringService from './Service/TCStringService';
+import {GVL} from '@iabtcf/core';
+import TCModelService from './Service/TCModelService';
 
 /**
  * SoloCmp.
@@ -12,9 +14,12 @@ import TCStringService from './Service/TCStringService';
 class SoloCmp {
 
     private _DependencyInjectionManager = DependencyInjectionManager;
+    private baseUrlVendorList: string;
     private readonly isDebugEnabled: boolean;
     private readonly cmpVersion: number;
     private readonly cmpVendorListVersion: number;
+    private cmpId: number;
+    private isServiceSpecific: boolean;
     private readonly tcStringCookieName: string;
     private readonly cmpConfig: any;
     private readonly supportedLanguages: string[];
@@ -28,6 +33,9 @@ class SoloCmp {
      * @param {number} cmpVersion
      * @param {number} cmpVendorListVersion
      * @param {string} tcStringCookieName
+     * @param {number} cmpId
+     * @param {boolean} isServiceSpecific
+     * @param {string} baseUrlVendorList
      */
     constructor(
         isDebugEnabled: boolean,
@@ -36,6 +44,9 @@ class SoloCmp {
         cmpVersion: number,
         cmpVendorListVersion: number,
         tcStringCookieName: string,
+        cmpId: number,
+        isServiceSpecific: boolean,
+        baseUrlVendorList: string,
     ) {
 
         this.isDebugEnabled = isDebugEnabled;
@@ -44,6 +55,9 @@ class SoloCmp {
         this.cmpVersion = cmpVersion;
         this.cmpVendorListVersion = cmpVendorListVersion;
         this.tcStringCookieName = tcStringCookieName;
+        this.cmpId = cmpId;
+        this.isServiceSpecific = isServiceSpecific;
+        this.baseUrlVendorList = baseUrlVendorList;
 
         this.registerServices();
         this.registerSubscribers();
@@ -85,6 +99,22 @@ class SoloCmp {
                     this.cmpVersion,
                     this.cmpVendorListVersion,
                     this.tcStringCookieName,
+                );
+
+            })
+            .addServiceProvider(TCModelService.name, (container: IContainer) => {
+
+                GVL.baseUrl = this.baseUrlVendorList;
+
+                const gvl: GVL = new GVL();
+
+                return new TCModelService(
+                    container[TCStringService.name],
+                    container[CmpSupportedLanguageProvider.name],
+                    this.cmpId,
+                    this.cmpVersion,
+                    this.isServiceSpecific,
+                    gvl,
                 );
 
             });
