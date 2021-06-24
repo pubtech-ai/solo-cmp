d96a8efa4995a75863ec623b89123dbb94a3689c
Marco Prontera <prontera.marco.pm@gmail.com>
2021-06-24 14:21:42 +0200

    Add util HttpRequest

---
+++ src/Util/HttpRequest.ts
@@ -0,0 +1,56 @@
+/**
+ * HttpRequest.
+ */
+class HttpRequest {
+
+    /**
+     * Make a request by the provided method and url.
+     *
+     * @param {string} method
+     * @param {string} url
+     * @private
+     *
+     * @return {Promise<object>}
+     */
+    public static makeRequest(method: string, url: string): Promise<any> {
+
+        return new Promise(function(resolve, reject) {
+
+            const xhr = new XMLHttpRequest();
+            xhr.open(method, url);
+
+            xhr.onload = function() {
+
+                if (this.status >= 200 && this.status < 300) {
+
+                    resolve(xhr);
+
+                } else {
+
+                    reject({
+                        status: this.status,
+                        statusText: xhr.statusText,
+                    });
+
+                }
+
+            };
+
+            xhr.onerror = function() {
+
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText,
+                });
+
+            };
+
+            xhr.send();
+
+        });
+
+    }
+
+}
+
+export default HttpRequest;
