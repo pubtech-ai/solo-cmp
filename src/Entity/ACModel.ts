3dde74013117addedc634d0cb46050fa4b997043
Marco Prontera <prontera.marco.pm@gmail.com>
2021-06-24 14:21:58 +0200

    Add ACModel

---
+++ src/Entity/ACModel.ts
@@ -0,0 +1,45 @@
+import GoogleVendorOption from './GoogleVendorOption';
+
+/**
+ * ACModel.
+ */
+class ACModel {
+
+    private _googleVendorOptions: GoogleVendorOption[] = [];
+
+    /**
+     * Constructor.
+     *
+     * @param {GoogleVendorOption[]} googleVendorOption
+     */
+    constructor(googleVendorOption: GoogleVendorOption[]) {
+
+        this._googleVendorOptions = googleVendorOption;
+
+    }
+
+    /**
+     * Return an array of GoogleVendorOption.
+     *
+     * @return {GoogleVendorOption[]}
+     */
+    get googleVendorOptions(): GoogleVendorOption[] {
+
+        return this._googleVendorOptions;
+
+    }
+
+    /**
+     * Set the provided array of GoogleVendorOption.
+     *
+     * @param {GoogleVendorOption[]} value
+     */
+    set googleVendorOptions(value: GoogleVendorOption[]) {
+
+        this._googleVendorOptions = value;
+
+    }
+
+}
+
+export default ACModel;
