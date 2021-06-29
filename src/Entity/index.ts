01bcf8e59dbfface4db262a636ac16f411d2d6df
Marco Prontera <prontera.marco.pm@gmail.com>
2021-06-29 12:13:50 +0200

    Update entity index exports

--- src/Entity/index.ts
+++ src/Entity/index.ts
@@ -1,2 +1,6 @@
 export * from './ACModel';
 export * from './GoogleVendorOption';
+export * from './VendorPurpose';
+export * from './VendorFeature';
+export * from './VendorOption';
+export * from './PurposeOption';
