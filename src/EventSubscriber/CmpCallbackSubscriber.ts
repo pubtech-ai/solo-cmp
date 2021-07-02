39260fe08e2287ad3d95c6b05d69bbb8d1b98258
Marco Prontera <prontera.marco.pm@gmail.com>
2021-07-02 10:40:10 +0200

    Add subscribers

---
+++ src/EventSubscriber/CmpCallbackSubscriber.ts
@@ -0,0 +1,51 @@
+import ConsentReadyEvent from '../Event/ConsentReadyEvent';
+import CmpConfiguration from '../Service/CmpConfiguration/CmpConfiguration';
+import EventSubscriberInterface from '../EventDispatcher/EventSubscriberInterface';
+import CmpConfigurationProvider from '../Service/CmpConfigurationProvider';
+
+/**
+ * CmpCallbackSubscriber.
+ */
+class CmpCallbackSubscriber implements EventSubscriberInterface {
+
+    private cmpConfigurationProvider: CmpConfigurationProvider;
+
+    /**
+     * Constructor.
+     *
+     * @param {CmpConfigurationProvider} cmpConfigurationProvider
+     */
+    constructor(cmpConfigurationProvider: CmpConfigurationProvider) {
+
+        this.cmpConfigurationProvider = cmpConfigurationProvider;
+
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public getSubscribedEvents(): Record<string, string> {
+
+        return {
+            [ConsentReadyEvent.name]: 'onConsentReady',
+        };
+
+    }
+
+    /**
+     * When the consent is ready, we need to call
+     * the callback of the publisher.
+     *
+     * @param {ConsentReadyEvent} event
+     */
+    public onConsentReady(event: ConsentReadyEvent): void {
+
+        const cmpConfiguration: CmpConfiguration = this.cmpConfigurationProvider.cmpConfiguration;
+
+        cmpConfiguration.onConsentAdsCallBack(event);
+
+    }
+
+}
+
+export default CmpCallbackSubscriber;
