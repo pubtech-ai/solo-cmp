a5aa069d417c26531570fe72c006c7cceae723fe
Marco Prontera <50167854+Marco-Prontera@users.noreply.github.com>
2021-07-12 09:03:44 +0200

    [#38] Remove expanded from UIChoices (#39)
    * Add language support on gvl
    
    * Cosmetics
    
    * Remove expanded property

--- src/SoloCmp.ts
+++ src/SoloCmp.ts
@@ -145,9 +145,14 @@ export class SoloCmp {
 
                 const gvl: GVL = new GVL();
 
+                const cmpSupportedLanguageProvider: CmpSupportedLanguageProvider =
+                    container[CmpSupportedLanguageProvider.getClassName()];
+
+                gvl.changeLanguage(cmpSupportedLanguageProvider.getCurrentLanguageForCmp());
+
                 return new TCModelService(
                     container[TCStringService.getClassName()],
-                    container[CmpSupportedLanguageProvider.getClassName()],
+                    cmpSupportedLanguageProvider,
                     this.cmpId,
                     this.cmpVersion,
                     this.isServiceSpecific,
