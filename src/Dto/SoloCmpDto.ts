df0ebe4aa9e42d4cb7ea0502eb1a4c9b932da5c2
Marco Prontera <prontera.marco.pm@gmail.com>
2021-08-02 17:00:27 +0200

    Cosmetics

--- src/Dto/SoloCmpDto.ts
+++ src/Dto/SoloCmpDto.ts
@@ -4,17 +4,17 @@ import {UIConstructor} from '../UIConstructor';
  * SoloCmpDto.
  */
 export interface SoloCmpDto {
-    uiConstructor: UIConstructor,
-    isDebugEnabled: boolean,
-    cmpConfig: any,
-    supportedLanguages: string[],
-    cmpVersion: number,
-    cmpVendorListVersion: number,
-    tcStringCookieName: string,
-    acStringLocalStorageName: string,
-    cmpId: number,
-    isServiceSpecific: boolean,
-    baseUrlVendorList: string,
-    initialHeightAmpCmpUi: string | null,
-    enableBorderAmpCmpUi: boolean | null,
+    uiConstructor: UIConstructor;
+    isDebugEnabled: boolean;
+    cmpConfig: any;
+    supportedLanguages: string[];
+    cmpVersion: number;
+    cmpVendorListVersion: number;
+    tcStringCookieName: string;
+    acStringLocalStorageName: string;
+    cmpId: number;
+    isServiceSpecific: boolean;
+    baseUrlVendorList: string;
+    initialHeightAmpCmpUi: string | null;
+    enableBorderAmpCmpUi: boolean | null;
 }
