fd05a93ca53f7b26c9ebb5a59c96bea4a04de003
Marco Prontera <prontera.marco.pm@gmail.com>
2021-12-06 14:20:23 +0100

    Optimize code

--- src/UIChoicesBridge/UIChoicesBridgeDtoBuilder.ts
+++ src/UIChoicesBridge/UIChoicesBridgeDtoBuilder.ts
@@ -72,27 +72,7 @@ export class UIChoicesBridgeDtoBuilder {
      */
     private buildUIPurposeChoices(tcModel: TCModel): void {
 
-        const purposes = tcModel.gvl.purposes;
-
-        const purposesIds: string[] = Object.keys(purposes);
-
-        const purposeOptions: PurposeOption[] = [];
-
-        purposesIds.forEach((purposeId) => {
-
-            const purpose: Purpose = purposes[purposeId];
-            purposeOptions.push({
-                state: tcModel.purposeConsents.has(purpose.id),
-                id: Number(purpose.id),
-                title: purpose.name,
-                description: purpose.description,
-                legalDescription: purpose.descriptionLegal,
-                vendors: tcModel.gvl.getVendorsWithConsentPurpose(purpose.id),
-            });
-
-        });
-
-        this._UIPurposeChoices = purposeOptions;
+        this._UIPurposeChoices = this.buildUIConsentChoices(tcModel, tcModel.gvl.purposes, 'purposeConsents', 'getVendorsWithConsentPurpose');
 
     }
 
@@ -105,27 +85,47 @@ export class UIChoicesBridgeDtoBuilder {
      */
     private buildUISpecialFeatureOptInsChoices(tcModel: TCModel): void {
 
-        const specialFeatures = tcModel.gvl.specialFeatures;
+        this._UISpecialFeatureOptInsChoices = this.buildUIConsentChoices(tcModel, tcModel.gvl.specialFeatures, 'specialFeatureOptins', 'getVendorsWithSpecialFeature');
 
-        const specialFeaturesIds: string[] = Object.keys(specialFeatures);
+    }
 
-        const specialFeaturesOptions: SpecialFeatureOption[] = [];
+    /**
+     * Build UI Consent choices.
+     *
+     * @param {TCModel} tcModel
+     * @param {IntMap<Purpose> | IntMap<Feature>}tcfChoices
+     * @param {string} tcfModelStateChoices
+     * @param {string} tcfGVLMethod
+     * @private
+     *
+     * @return {PurposeOption[] | SpecialFeatureOption[]}
+     */
+    private buildUIConsentChoices(
+        tcModel,
+        tcfChoices,
+        tcfModelStateChoices,
+        tcfGVLMethod,
+    ): PurposeOption[] | SpecialFeatureOption[] {
 
-        specialFeaturesIds.forEach((specialFeaturesId) => {
+        const choicesIds: string[] = Object.keys(tcfChoices);
 
-            const specialFeature: Feature = specialFeatures[specialFeaturesId];
-            specialFeaturesOptions.push({
-                state: tcModel.specialFeatureOptins.has(specialFeature.id),
-                id: Number(specialFeature.id),
-                title: specialFeature.name,
-                description: specialFeature.description,
-                legalDescription: specialFeature.descriptionLegal,
-                vendors: tcModel.gvl.getVendorsWithSpecialFeature(specialFeature.id),
+        const consentOptions: PurposeOption[] | SpecialFeatureOption[] = [];
+
+        choicesIds.forEach((choiceId) => {
+
+            const choice: Purpose | Feature = tcfChoices[choiceId];
+            consentOptions.push({
+                state: tcModel[tcfModelStateChoices].has(choice.id),
+                id: Number(choice.id),
+                title: choice.name,
+                description: choice.description,
+                legalDescription: choice.descriptionLegal,
+                vendors: tcModel.gvl[tcfGVLMethod](choice.id),
             });
 
         });
 
-        this._UISpecialFeatureOptInsChoices = specialFeaturesOptions;
+        return consentOptions;
 
     }
 
@@ -142,47 +142,7 @@ export class UIChoicesBridgeDtoBuilder {
 
         const vendorIds: string[] = Object.keys(vendors);
 
-        const vendorOption: VendorOption[] = [];
-
-        vendorIds.forEach((vendorId) => {
-
-            const vendor: Vendor = vendors[vendorId];
-
-            // IAB use null instead of NaN, it's a bad design, so we fix it!
-            let cookieMaxAgeSeconds = NaN;
-
-            if (typeof vendor.cookieMaxAgeSeconds === 'number' && !isNaN(vendor.cookieMaxAgeSeconds)) {
-
-                cookieMaxAgeSeconds = Number(vendor.cookieMaxAgeSeconds);
-
-            }
-
-            // Set vendors default to true, purposes lead vendors
-            const state = this.firstTimeConsentRequest ? true : tcModel.vendorConsents.has(vendor.id);
-
-            vendorOption.push({
-                state: state,
-                features: UIChoicesBridgeDtoBuilder.buildVendorFeatures(vendor.features, tcModel.gvl.features),
-                flexiblePurposes: vendor.flexiblePurposes,
-                id: Number(vendor.id),
-                legIntPurposes: vendor.legIntPurposes,
-                name: vendor.name,
-                policyUrl: vendor.policyUrl,
-                cookieMaxAgeSeconds: cookieMaxAgeSeconds,
-                purposes: UIChoicesBridgeDtoBuilder.buildVendorPurposes(vendor.purposes, tcModel.gvl.purposes),
-                specialFeatures: UIChoicesBridgeDtoBuilder.buildVendorFeatures(
-                    vendor.specialFeatures,
-                    tcModel.gvl.specialFeatures,
-                ),
-                specialPurposes: UIChoicesBridgeDtoBuilder.buildVendorPurposes(
-                    vendor.specialPurposes,
-                    tcModel.gvl.specialPurposes,
-                ),
-            });
-
-        });
-
-        this._UIVendorChoices = vendorOption;
+        this._UIVendorChoices = this.buildUIConsentVendorChoices(tcModel, vendorIds, vendors, 'vendorConsents');
 
     }
 
@@ -199,6 +159,88 @@ export class UIChoicesBridgeDtoBuilder {
         const allVendorsWithLegitimateInterests = this.buildLegitimateInterestsPurposeOptions(tcModel);
 
         this.buildLegitimateInterestsVendorOptions(allVendorsWithLegitimateInterests, tcModel);
+
+    }
+
+    /**
+     * Build the UILegitimateInterestsVendorChoices with the provided TCModel and set
+     * status in base of the consent enabled in it.
+     *
+     * @param {array} allVendorsWithLegitimateInterests
+     * @param {TCModel} tcModel
+     * @private
+     */
+    private buildLegitimateInterestsVendorOptions(
+        allVendorsWithLegitimateInterests: IntMap<Vendor>,
+        tcModel: TCModel,
+    ): void {
+
+        const vendorIds = Object.keys(allVendorsWithLegitimateInterests);
+
+        this._UILegitimateInterestsVendorChoices = this.buildUIConsentVendorChoices(tcModel, vendorIds, allVendorsWithLegitimateInterests, 'vendorLegitimateInterests');
+
+    }
+
+    /**
+     * Build UI Consent for Vendor Choices.
+     *
+     * @param {TCModel} tcModel
+     * @param {string[]} vendorIds
+     * @param {IntMap<Vendor>} allVendorsWithLegitimateInterests
+     * @param {string} tcfModelStateChoices
+     * @private
+     *
+     * @return {VendorOption[]}
+     */
+    private buildUIConsentVendorChoices(
+        tcModel: TCModel,
+        vendorIds: string[],
+        allVendorsWithLegitimateInterests: IntMap<Vendor>,
+        tcfModelStateChoices: string): VendorOption[] {
+
+        const consentVendorOptions: VendorOption[] = [];
+
+        vendorIds.forEach((vendorId) => {
+
+            const vendor: Vendor = allVendorsWithLegitimateInterests[vendorId];
+
+            // IAB use null instead of NaN, it's a bad design, so we fix it!
+            let cookieMaxAgeSeconds = NaN;
+
+            if (typeof vendor.cookieMaxAgeSeconds === 'number' && !isNaN(vendor.cookieMaxAgeSeconds)) {
+
+                cookieMaxAgeSeconds = Number(vendor.cookieMaxAgeSeconds);
+
+            }
+
+            // Legitimate interest choices are opt-out, so the default is true
+            const state = this.firstTimeConsentRequest ? true : tcModel[tcfModelStateChoices].has(vendor.id);
+
+            consentVendorOptions.push({
+                state: state,
+                features: UIChoicesBridgeDtoBuilder
+                    .buildVendorPurposesOrVendorFeatures(vendor.features, tcModel.gvl.features),
+                flexiblePurposes: vendor.flexiblePurposes,
+                id: Number(vendor.id),
+                legIntPurposes: vendor.legIntPurposes,
+                name: vendor.name,
+                policyUrl: vendor.policyUrl,
+                cookieMaxAgeSeconds: cookieMaxAgeSeconds,
+                purposes: UIChoicesBridgeDtoBuilder
+                    .buildVendorPurposesOrVendorFeatures(vendor.purposes, tcModel.gvl.purposes),
+                specialFeatures: UIChoicesBridgeDtoBuilder.buildVendorPurposesOrVendorFeatures(
+                    vendor.specialFeatures,
+                    tcModel.gvl.specialFeatures,
+                ),
+                specialPurposes: UIChoicesBridgeDtoBuilder.buildVendorPurposesOrVendorFeatures(
+                    vendor.specialPurposes,
+                    tcModel.gvl.specialPurposes,
+                ),
+            });
+
+        });
+
+        return consentVendorOptions;
 
     }
 
@@ -253,114 +295,29 @@ export class UIChoicesBridgeDtoBuilder {
     }
 
     /**
-     * Build the UILegitimateInterestsVendorChoices with the provided TCModel and set
-     * status in base of the consent enabled in it.
-     *
-     * @param {array} allVendorsWithLegitimateInterests
-     * @param {TCModel} tcModel
-     * @private
-     */
-    private buildLegitimateInterestsVendorOptions(
-        allVendorsWithLegitimateInterests: IntMap<Vendor>,
-        tcModel: TCModel,
-    ): void {
-
-        const legitimateInterestsVendorOption: VendorOption[] = [];
-
-        const vendorIds = Object.keys(allVendorsWithLegitimateInterests);
-
-        vendorIds.forEach((vendorId) => {
-
-            const vendor: Vendor = allVendorsWithLegitimateInterests[vendorId];
-
-            // IAB use null instead of NaN, it's a bad design, so we fix it!
-            let cookieMaxAgeSeconds = NaN;
-
-            if (typeof vendor.cookieMaxAgeSeconds === 'number' && !isNaN(vendor.cookieMaxAgeSeconds)) {
-
-                cookieMaxAgeSeconds = Number(vendor.cookieMaxAgeSeconds);
-
-            }
-
-            // Legitimate interest choices are opt-out, so the default is true
-            const state = this.firstTimeConsentRequest ? true : tcModel.vendorLegitimateInterests.has(vendor.id);
-
-            legitimateInterestsVendorOption.push({
-                state: state,
-                features: UIChoicesBridgeDtoBuilder.buildVendorFeatures(vendor.features, tcModel.gvl.features),
-                flexiblePurposes: vendor.flexiblePurposes,
-                id: Number(vendor.id),
-                legIntPurposes: vendor.legIntPurposes,
-                name: vendor.name,
-                policyUrl: vendor.policyUrl,
-                cookieMaxAgeSeconds: cookieMaxAgeSeconds,
-                purposes: UIChoicesBridgeDtoBuilder.buildVendorPurposes(vendor.purposes, tcModel.gvl.purposes),
-                specialFeatures: UIChoicesBridgeDtoBuilder.buildVendorFeatures(
-                    vendor.specialFeatures,
-                    tcModel.gvl.specialFeatures,
-                ),
-                specialPurposes: UIChoicesBridgeDtoBuilder.buildVendorPurposes(
-                    vendor.specialPurposes,
-                    tcModel.gvl.specialPurposes,
-                ),
-            });
-
-        });
-
-        this._UILegitimateInterestsVendorChoices = legitimateInterestsVendorOption;
-
-    }
-
-    /**
      * Build the vendor purpose entity for every purposes provided.
      *
-     * @param {number[]} purposeIds
-     * @param {IntMap<Purpose>} purposesBases
+     * @param {number[]} purposeOrFeatureIds
+     * @param {IntMap<Purpose>|IntMap<Feature>} purposesOrFeatureBases
      * @private
      *
-     * @return {VendorPurpose[]}
+     * @return {VendorPurpose[]|VendorFeature[]}
      */
-    private static buildVendorPurposes(purposeIds: number[], purposesBases: IntMap<Purpose>): VendorPurpose[] {
+    private static buildVendorPurposesOrVendorFeatures(
+        purposeOrFeatureIds: number[],
+        purposesOrFeatureBases: IntMap<Purpose> | IntMap<Feature>,
+    ): VendorPurpose[] | VendorFeature[] {
 
-        const result: VendorPurpose[] = [];
+        const result: VendorPurpose[] | VendorFeature[] = [];
 
-        purposeIds.forEach((purposeId) => {
+        purposeOrFeatureIds.forEach((purposeId) => {
 
-            const purpose = purposesBases[purposeId];
+            const purpose = purposesOrFeatureBases[purposeId];
             result.push({
                 id: Number(purpose.id),
                 name: purpose.name,
                 description: purpose.description,
                 descriptionLegal: purpose.descriptionLegal,
-            });
-
-        });
-
-        return result;
-
-    }
-
-    /**
-     * Build the vendor feature entity for every feature provided.
-     *
-     * @param {number[]} featureIds
-     * @param {IntMap<Feature>} featuresBases
-     * @private
-     *
-     * @return {VendorPurpose[]}
-     */
-    private static buildVendorFeatures(featureIds: number[], featuresBases: IntMap<Feature>): VendorFeature[] {
-
-        const result: VendorFeature[] = [];
-
-        featureIds.forEach((featureId) => {
-
-            const feature = featuresBases[featureId];
-            result.push({
-                id: Number(feature.id),
-                name: feature.name,
-                description: feature.description,
-                descriptionLegal: feature.descriptionLegal,
             });
 
         });
