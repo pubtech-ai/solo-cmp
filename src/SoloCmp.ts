519ccfe07109da24305d5193afa9e361d0490e60
Marco Prontera <50167854+Marco-Prontera@users.noreply.github.com>
2021-12-22 11:33:59 +0100

    [#92] When the isAmp parameter is true the init function must render the CMP (#93)
    * Fix
    
    * Add comment
    
    * Add explanation
    
    * 2.0.1

--- src/SoloCmp.ts
+++ src/SoloCmp.ts
@@ -295,13 +295,31 @@ export class SoloCmp {
 
         const eventDispatcher = this._DependencyInjectionManager.getService(EventDispatcher.getClassName());
 
-        if (tcStringService.isValidTCString(tcString) && acStringService.isValidACString(acString) && (typeof additionalValidationCallback == 'function' ? additionalValidationCallback() : true)) {
+        const isAdditionalValidationCallbackValid: boolean = (typeof additionalValidationCallback == 'function' ? additionalValidationCallback() : true);
 
-            const consentReadyEvent = new ConsentReadyEvent(tcString as string, acString as string);
+        if (
+            tcStringService.isValidTCString(tcString) &&
+            acStringService.isValidACString(acString) &&
+            isAdditionalValidationCallbackValid) {
 
-            eventDispatcher.dispatch(consentReadyEvent);
+            if (this.isAmp) {
 
-            this.uiConstructor.buildOpenCmpButtonAndRender();
+                /**
+                 * We always need to render the CMP when the environment is AMP,
+                 * because the CMP will be executed only when AMP require consent
+                 * or the user want to change their privacy settings.
+                 */
+                eventDispatcher.dispatch(new OpenCmpUIEvent(tcString as string, acString as string));
+
+            } else {
+
+                const consentReadyEvent = new ConsentReadyEvent(tcString as string, acString as string);
+
+                eventDispatcher.dispatch(consentReadyEvent);
+
+                this.uiConstructor.buildOpenCmpButtonAndRender();
+
+            }
 
         } else {
 
