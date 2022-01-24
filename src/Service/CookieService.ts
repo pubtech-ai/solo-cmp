91af9ac2b06736930dabad739e5d304237a6b9d0
Marco Prontera <prontera.marco.pm@gmail.com>
2022-01-24 14:02:50 +0100

    Switch error to debug

--- src/Service/CookieService.ts
+++ src/Service/CookieService.ts
@@ -63,7 +63,8 @@ export class CookieService {
 
         } catch (error) {
 
-            this.logger.error('decodeURIComponent error when read cookie, fallback to document.cookie.', error);
+            this.logger.debug('decodeURIComponent error when read cookie, fallback to document.cookie.', error);
+            
 
         }
 
